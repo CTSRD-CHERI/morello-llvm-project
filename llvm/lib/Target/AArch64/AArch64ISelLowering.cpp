5d87e02da8a91f5c2ab28bec76766baa4c99bf1a
John Baldwin <jhb@FreeBSD.org>
2025-02-17 12:50:24 -0500

    HACK: Force GOT access for all global addresses in purecap.
    This requires preserving symbol names for additional Morello
    relocations used for GOT access.
    
    The changes in the c18n-fptr do this conditionally for function
    pointers, but cross-compartment data access requires this for other
    pointers as well.  This matches the behavior on CHERI RISC-V.

--- llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
+++ llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -8035,6 +8035,10 @@ SDValue AArch64TargetLowering::LowerGlobalAddress(SDValue Op,
                                    getGOT(GN, DAG, OpFlags);
   }
 
+  if (Op.getSimpleValueType() == MVT::iFATPTR128 && !IsLargeCM &&
+      Subtarget->hasC64())
+    return getFatGOT(GN, DAG, OpFlags);
+
   if (!IsLargeCM && Op.getSimpleValueType() == MVT::iFATPTR128) {
     MachineFunction &MF = DAG.getMachineFunction();
     AArch64FunctionInfo *FuncInfo = MF.getInfo<AArch64FunctionInfo>();
--- llvm/lib/Target/AArch64/MCTargetDesc/AArch64ELFObjectWriter.cpp
+++ llvm/lib/Target/AArch64/MCTargetDesc/AArch64ELFObjectWriter.cpp
@@ -51,6 +51,8 @@ bool AArch64ELFObjectWriter::needsRelocateWithSymbol(const MCSymbol &Sym,
   default:
     return false;
 
+  case ELF::R_MORELLO_ADR_GOT_PAGE:
+  case ELF::R_MORELLO_LD128_GOT_LO12_NC:
   case ELF::R_MORELLO_CAPINIT:
   case ELF::R_MORELLO_DESC_CAPINIT:
   case ELF::R_MORELLO_CODE_CAPINIT:
