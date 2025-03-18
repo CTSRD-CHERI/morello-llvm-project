393b09a6017d7ab1372ac67375c5c32c170d576a
John Baldwin <jhb@FreeBSD.org>
2025-03-18 13:02:32 -0400

    lld: handleCrossCompartmentCall: Handle symbols without a compartment
    This can happen due to --just-symbols.  These are effectively an
    undefined symbol even though they are marked as defined.

--- lld/ELF/Relocations.cpp
+++ lld/ELF/Relocations.cpp
@@ -1801,7 +1801,9 @@ static bool handleCrossCompartmentCall(Compartment *c, Symbol &sym) {
     return false;
 
   auto symCompartment = sym.containingCompartment();
-  assert (symCompartment);
+  if (!symCompartment)
+    return false;
+
   if (*symCompartment == c)
     return false;
 
