95d610b7c15193029bb2faf1ed9e722b383c2677
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-05-28 14:31:30 -0700

    [ELF] Fix old mismerge that caused test to fail

--- lld/test/ELF/cheri/cap-table/cap-table-global-init.c
+++ lld/test/ELF/cheri/cap-table/cap-table-global-init.c
@@ -1,5 +1,6 @@
 // REQUIRES: mips
-// RUN: %cheri128_purecap_cc1 -pedantic -Wextra %s -emit-obj -mllvm -mxgot -mllvm -cheri-cap-table-abi=plt -x c -O0 -o %t.o
+
+// RUN: %cheri128_purecap_cc1 -pedantic -Wextra %s -emit-obj -mllvm -cheri-cap-table-abi=plt -x c -O0 -o %t.o
 // RUN: ld.lld %t.o -o %t.exe
 // RUN: llvm-objdump -t %t.exe | FileCheck %s
 
