d1feec65a157d356824d3ddfdfb86553eaf0f739
Alex Richardson <Alexander.Richardson@cl.cam.ac.uk>
2025-05-30 16:58:27 -0700

    Update a few more morello tests for the new output format

--- lld/test/ELF/morello/morello-adrp_prel_pg_hi20-within-range.s
+++ lld/test/ELF/morello/morello-adrp_prel_pg_hi20-within-range.s
@@ -3,8 +3,9 @@
 // RUN: ld.lld %t.o -o %t
 // RUN: llvm-objdump --mattr=+morello -d %t | FileCheck %s
 
-// CHECK: adrp c25, 0x8020f000 <foo+0x1ffffc>
-// CHECK: adrp c26, 0xffffffff80210000
+// CHECK: 0000000000210120 <foo>:
+// CHECK-NEXT: adrp c25, 0x8020f000 <foo+0x7fffeee0>
+// CHECK-NEXT: adrp c26, 0xffffffff80210000 <foo+0xffffffff7ffffee0>
 
   .text
   .global _start
