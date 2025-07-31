8becac4b5d75f09a2a39c1a197ac9c7f63d37efa
Jessica Clarke <jrtc27@jrtc27.com>
2025-07-31 02:17:43 +0100

    [NFC][ELF][Morello] Add test showing broken IFUNC canonical PLT VA
    Canonical PLTs for non-preemptible IFUNCs are currently A64 symbols not
    C64 ones.

--- lld/test/ELF/morello/aarch64-c64-gnu-ifunc.s
+++ lld/test/ELF/morello/aarch64-c64-gnu-ifunc.s
@@ -16,47 +16,66 @@ foo:
 bar:
  ret
 
+.type baz STT_GNU_IFUNC
+.globl baz
+.size baz, 4
+baz:
+ ret
+
 .globl _start
 _start:
  bl foo
  bl bar
+ adr c0, baz
  add x2, x2, :lo12:__rela_iplt_start
  add x2, x2, :lo12:__rela_iplt_end
 
 // Contents of section .got.plt:
-// CHECK:      220270 c0012000 00000000 00010200 00000004
-// CHECK-NEXT: 220280 c0012000 00000000 00010200 00000004
+// CHECK:      2202a0 c0012000 00000000 40010200 00000004
+// CHECK-NEXT: 2202b0 c0012000 00000000 40010200 00000004
+// CHECK-NEXT: 2202c0 c0012000 00000000 40010200 00000004
 
 // CHECK-LABEL: <foo>:
-// CHECK-NEXT:   210239:        <unknown>
+// CHECK-NEXT:   210251:        <unknown>
 
 // CHECK-LABEL: <bar>:
-// CHECK-NEXT:   21023d:        <unknown>
+// CHECK-NEXT:   210255:        <unknown>
+
+/// No label; baz is redirected to the .iplt entry
+// CHECK-NEXT:   210259:        <unknown>
 
 // CHECK-LABEL: <_start>:
-// CHECK-NEXT:   210240:  bl      0x210250
-// CHECK-NEXT:            bl      0x210260
+// CHECK-NEXT:   21025c:  bl      0x210270
+// CHECK-NEXT:            bl      0x210280
+/// TODO: Missing C64 bit
+// CHECK-NEXT:            adr     c0, 0x210290
 // CHECK-NEXT:            add     x2, x2, #0x208
-// CHECK-NEXT:            add     x2, x2, #0x238
+// CHECK-NEXT:            add     x2, x2, #0x250
 
 // CHECK-LABEL: <.iplt>:
-// CHECK-NEXT:   210250:  adrp c16, 0x220000
-// CHECK-NEXT:            add  c16, c16, #0x270
+// CHECK-NEXT:   210270:  adrp c16, 0x220000
+// CHECK-NEXT:            add  c16, c16, #0x2a0
 // CHECK-NEXT:            ldr  c17, [c16, #0x0]
 // CHECK-NEXT:            br   c17
 // CHECK-NEXT:            adrp c16, 0x220000
-// CHECK-NEXT:            add  c16, c16, #0x280
+// CHECK-NEXT:            add  c16, c16, #0x2b0
+// CHECK-NEXT:            ldr  c17, [c16, #0x0]
+// CHECK-NEXT:            br   c17
+// CHECK-LABEL: <baz>:
+// CHECK-NEXT:   210290:  adrp c16, 0x220000
+// CHECK-NEXT:            add  c16, c16, #0x2c0
 // CHECK-NEXT:            ldr  c17, [c16, #0x0]
 // CHECK-NEXT:            br   c17
 
 // RELANDSYM: Relocations [
 // RELANDSYM-NEXT:   Section {{.*}} .rela.dyn {
-// RELANDSYM-NEXT:     0x220270 R_MORELLO_IRELATIVE - 0x10079
-// RELANDSYM-NEXT:     0x220280 R_MORELLO_IRELATIVE - 0x1007D
+// RELANDSYM-NEXT:     0x2202A0 R_MORELLO_IRELATIVE - 0x10091
+// RELANDSYM-NEXT:     0x2202B0 R_MORELLO_IRELATIVE - 0x10095
+// RELANDSYM-NEXT:     0x2202C0 R_MORELLO_IRELATIVE - 0x10099
 
 // RELANDSYM:          Name: __rela_iplt_start
 // RELANDSYM-NEXT:     Value: 0x200208
-// RELANDSYM-NEXT:     Size: 48
+// RELANDSYM-NEXT:     Size: 72
 // RELANDSYM-NEXT:     Binding: Local (0x0)
 // RELANDSYM-NEXT:     Type: None (0x0)
 // RELANDSYM-NEXT:     Other [ (0x2)
@@ -66,7 +85,7 @@ _start:
 // RELANDSYM-NEXT:   }
 // RELANDSYM-NEXT:   Symbol {
 // RELANDSYM-NEXT:     Name: __rela_iplt_end
-// RELANDSYM-NEXT:     Value: 0x200238
+// RELANDSYM-NEXT:     Value: 0x200250
 // RELANDSYM-NEXT:     Size: 0
 // RELANDSYM-NEXT:     Binding: Local (0x0)
 // RELANDSYM-NEXT:     Type: None (0x0)
