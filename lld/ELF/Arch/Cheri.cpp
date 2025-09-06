CTSRD-CHERI/morello-llvm-project

commit f51788dbea710ed44c779f2c64d33ca115c149c6

[NFCI][ELF][Morello] Implicitly set localCapRelocsMode for dynamic objects
Now that we properly implement CapRelocsMode::ElfReloc for Morello we
should just use that for the dynamic case rather than leave it
pretending that we're using legacy caprelocs when we're not.

Note that the getTargetSize condition should have been hasDynSymTab as
it was in addMorelloRelativeRelocation; otherwise this will read 8 bytes
past the end of the fragment if producing a dynamically-linked
executable, which is often, but not always, 0, and may even be out of
bounds if at the end of the data section.

lld/ELF/Arch/Cheri.cpp
@@ -448,8 +448,7 @@ static uint64_t getTargetSize(const CheriCapRelocLocation &location,
       assert(!location.section->compressed);
       const uint8_t *buf =
           location.section->content().begin() + location.offset;
-      targetSize = ((config->localCapRelocsMode == CapRelocsMode::Legacy) &&
-                    !config->shared)
+      targetSize = config->localCapRelocsMode == CapRelocsMode::Legacy
                        ? read64le(buf + 8)
                        : read64le(buf);
       if (targetSize != 0) {
@@ -1021,9 +1020,7 @@ static void addCapDynamicRelocation(RelType dynType, Symbol *sym,
 void addMorelloRelativeRelocation(RelType dynType, Symbol *sym,
                                   InputSectionBase *sec, uint64_t offset,
                                   int64_t addend) {
-  // If there is a Dynamic Symbol Table, there cannot be a caprelocs section.
-  if (config->localCapRelocsMode == CapRelocsMode::ElfReloc ||
-      config->hasDynSymTab) {
+  if (config->localCapRelocsMode == CapRelocsMode::ElfReloc) {
     addCapDynamicRelocation(dynType, sym, sec, offset, addend);
   } else {
     in.morelloCapRelocs->addCapReloc({sec, offset}, {sym, 0u}, sym->isPreemptible,

lld/ELF/Driver.cpp
@@ -3108,6 +3108,8 @@ void LinkerDriver::link(opt::InputArgList &args) {
     if (config->isCheriFnDesc)
       config->localCapRelocsMode = CapRelocsMode::ElfReloc;
   }
+  if (config->emachine == EM_AARCH64 && config->hasDynSymTab)
+    config->localCapRelocsMode = CapRelocsMode::ElfReloc;
 
   // The Target instance handles target-specific stuff, such as applying
   // relocations or writing a PLT section. It also contains target-dependent

commit 7d219c6ec3382ae31fc24915aeae65c0f63f7793

[NFCI][ELF][Morello] Drop a lingering hasDynSymTab check
This is tautological now useRelativeElfCheriRelocs is implied by
hasDynSymTab.

Fixes: f51788dbea71 ("[NFCI][ELF][Morello] Implicitly set localCapRelocsMode for dynamic objects")

lld/ELF/Arch/Cheri.cpp
@@ -801,7 +801,7 @@ static void addMorelloRelativeElfReloc(RelType dynType, Symbol *sym,
                                        InputSectionBase *sec, uint64_t offset,
                                        int64_t addend) {
   assert(dynType == R_MORELLO_RELATIVE || dynType == R_MORELLO_FUNC_RELATIVE);
-  assert(config->useRelativeElfCheriRelocs || config->hasDynSymTab);
+  assert(config->useRelativeElfCheriRelocs);
 
   if (dynType == R_MORELLO_RELATIVE && config->isCheriFnDesc) {
     bool isDescSym = false;