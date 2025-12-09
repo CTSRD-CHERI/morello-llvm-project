rems-project/llvm-project

commit 340b42eebe59fe5a9201d6daf456edb31c94890d

[Morello] Add lld support for the descriptor ABI.
This is a rebased version of the the original patch series authored
by Amilendra Kodithuwakku with the following changes:
  - Fixed instructions in the PLT have been changed to use adrdp
  - Removed global entry point thunks in accordance with the
    latest version of the specification.

Co-authored-by: Amilendra Kodithuwakku <amilendra.kodithuwakku@arm.com>

lld/ELF/SyntheticSections.h
@@ -1215,7 +1215,8 @@ template <typename ELFT> void writePhdrs(uint8_t *buf, Partition &part);
 
 Defined *addSyntheticLocal(StringRef name, uint8_t type, uint64_t value,
                            uint64_t size, InputSectionBase &section);
-
+Defined *addGlobalFunc(Symbol &sym, uint64_t value, uint64_t size,
+                       InputSectionBase &section);
 void addVerneed(Symbol *ss);
 
 // Linker generated per-partition sections.

lld/ELF/Arch/AArch64.cpp
@@ -6,6 +6,7 @@
 //
 //===----------------------------------------------------------------------===//
 
+#include "OutputSections.h"
 #include "Symbols.h"
 #include "SyntheticSections.h"
 #include "Target.h"
@@ -182,6 +183,10 @@ RelExpr AArch64::getRelExpr(RelType type, const Symbol &s,
   case R_AARCH64_JUMP26:
   case R_AARCH64_TSTBR14:
   case R_AARCH64_PLT32:
+  case R_MORELLO_DESC_GLOBAL_CALL26:
+  case R_MORELLO_DESC_GLOBAL_JUMP26:
+  case R_AARCH64_DESC_GLOBAL_CALL26:
+  case R_AARCH64_DESC_GLOBAL_JUMP26:
     return R_PLT_PC;
   case R_AARCH64_PREL16:
   case R_AARCH64_PREL32:
@@ -201,10 +206,13 @@ RelExpr AArch64::getRelExpr(RelType type, const Symbol &s,
   case R_MORELLO_ADR_PREL_PG_HI20:
   case R_MORELLO_ADR_PREL_PG_HI20_NC:
     return R_AARCH64_PAGE_PC;
+  case R_MORELLO_DESC_ADR_PREL_PG_HI20:
+    return s.isDefined() ? R_MORELLO_DESC_PAGE_PC : R_AARCH64_PAGE_PC;
   case R_AARCH64_LD64_GOT_LO12_NC:
   case R_AARCH64_TLSIE_LD64_GOTTPREL_LO12_NC:
   case R_MORELLO_LD128_GOT_LO12_NC:
   case R_MORELLO_TLSIE_ADD_LO12:
+  case R_MORELLO_DESC_LD128_GOT_LO12_NC:
     return R_GOT;
   case R_AARCH64_LD64_GOTPAGE_LO15:
     return R_AARCH64_GOT_PAGE;
@@ -213,10 +221,14 @@ RelExpr AArch64::getRelExpr(RelType type, const Symbol &s,
   case R_AARCH64_TLSIE_ADR_GOTTPREL_PAGE21:
   case R_MORELLO_TLSIE_ADR_GOTTPREL_PAGE20:
     return R_AARCH64_GOT_PAGE_PC;
+  case R_MORELLO_DESC_ADR_GOT_PAGE:
+    return R_MORELLO_DESC_GOT_PAGE_PC;
   case R_AARCH64_NONE:
     return R_NONE;
   case R_MORELLO_CAPINIT:
     return R_CHERI_CAPABILITY;
+  case R_MORELLO_DESC_CAPINIT:
+    return R_MORELLO_DESC_CAPABILITY;
   case R_MORELLO_LD_PREL_LO17:
     return R_MORELLO_VADREF;
   default:
@@ -269,6 +281,7 @@ bool AArch64::usesOnlyLowPageBits(RelType type) const {
   case R_MORELLO_TLSIE_ADD_LO12:
   case R_MORELLO_LD128_GOT_LO12_NC:
   case R_MORELLO_TLSDESC_LD128_LO12:
+  case R_MORELLO_DESC_LD128_GOT_LO12_NC:
     return true;
   }
 }
@@ -482,6 +495,14 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
     checkInt(loc, val, 21, rel);
     write32Addr(loc, val, 0x1FFFFC);
     break;
+  case R_MORELLO_DESC_GLOBAL_CALL26:
+  case R_MORELLO_DESC_GLOBAL_JUMP26:
+  case R_AARCH64_DESC_GLOBAL_CALL26:
+  case R_AARCH64_DESC_GLOBAL_JUMP26:
+    // if not in PLT jump over the first instruction for the desc relocations
+    if (rel.sym && rel.sym->type == STT_FUNC && !rel.sym->needsPlt)
+      val += 4;
+    LLVM_FALLTHROUGH;
   case R_MORELLO_CALL26:
   case R_MORELLO_JUMP26:
     // If bit 0 is clear then our target is in A64 state, interworking thunks
@@ -557,6 +578,7 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
   case R_AARCH64_TLSLE_LDST128_TPREL_LO12_NC:
   case R_MORELLO_LD128_GOT_LO12_NC:
   case R_MORELLO_TLSDESC_LD128_LO12:
+  case R_MORELLO_DESC_LD128_GOT_LO12_NC:
     checkAlignment(loc, val, 16, rel);
     or32AArch64Imm(loc, getBits(val, 4, 11));
     break;
@@ -637,6 +659,13 @@ void AArch64::relocate(uint8_t *loc, const Relocation &rel,
   case R_AARCH64_TLSDESC:
     // For R_AARCH64_TLSDESC the addend is stored in the second 64-bit word.
     write64(loc + 8, val);
+    break;
+  case R_MORELLO_DESC_ADR_PREL_PG_HI20:
+  case R_MORELLO_DESC_ADR_GOT_PAGE:
+    // Reset bit 23 (P) to convert the ADRP to ADRDP
+    write32le(loc, (read32le(loc) & ~(1 << 23)));
+    // Setting the immediate is same as the ADRP
+    relocateNoSym(loc, R_MORELLO_ADR_PREL_PG_HI20, val);
     break;
   default:
     llvm_unreachable("unknown relocation");
@@ -1034,7 +1063,7 @@ void AArch64BtiPac::writePlt(uint8_t *buf, const Symbol &sym,
 }
 
 namespace {
-class AArch64C64 final : public AArch64 {
+class AArch64C64 : public AArch64 {
 public:
   AArch64C64();
   void writePltHeader(uint8_t *buf) const override;
@@ -1049,6 +1078,14 @@ public:
                       uint64_t val) const override;
 private:
   const uint8_t *getPltBranchR17() const;
+};
+
+class AArch64C64DescABI final : public AArch64C64 {
+public:
+  AArch64C64DescABI();
+  void writePltHeader(uint8_t *buf) const override;
+  void writePlt(uint8_t *buf, const Symbol &sym,
+                uint64_t pltEntryAddr) const override;
 };
 } // namespace
 
@@ -1217,10 +1254,58 @@ void AArch64C64::relaxTlsIeToLe(uint8_t *loc, const Relocation &rel,
   }
 }
 
+AArch64C64DescABI::AArch64C64DescABI() {
+  pltRel = R_MORELLO_DESC_JUMP_SLOT;
+  gotRel = R_MORELLO_DESC_GLOB_DAT;
+  iRelativeRel = R_MORELLO_DESC_IRELATIVE;
+}
+
+void AArch64C64DescABI::writePltHeader(uint8_t *buf) const {
+  const uint8_t pltData[] = {
+      0xf0, 0x7b, 0xbf, 0x62, // stp  c16, c30, [csp, #-32]!
+      0x10, 0x00, 0x80, 0x90, // adrp c16, Page(&(.plt.got[2]))
+      0x11, 0x02, 0x40, 0xc2, // ldr  c29, [c16, Offset(&(.plt.got[2]))]
+      0x10, 0x02, 0x00, 0x02, // add  c16, c16, Offset(&(.plt.got[2]))
+      0x1d, 0x12, 0xc4, 0xc2, // ldpbr	c29, [c16]
+      0x1f, 0x20, 0x03, 0xd5, // nop
+      0x1f, 0x20, 0x03, 0xd5, // nop
+      0x1f, 0x20, 0x03, 0xd5, // nop
+  };
+
+  memcpy(buf, pltData, sizeof(pltData));
+
+  uint64_t got = in.gotPlt->getVA();
+  relocateNoSym(buf + 4, R_MORELLO_DESC_ADR_PREL_PG_HI20,
+                getAArch64Page(got + 32) - Out::descPhdr->firstSec->addr);
+  relocateNoSym(buf + 8, R_AARCH64_LDST128_ABS_LO12_NC, got + 32);
+  relocateNoSym(buf + 12, R_AARCH64_ADD_ABS_LO12_NC, got + 32);
+}
+
+void AArch64C64DescABI::writePlt(uint8_t *buf, const Symbol &sym,
+                                 uint64_t pltEntryAddr) const {
+  const uint8_t pltData[] = {
+      0x10, 0x00, 0x00, 0x90, // adrdp  c16, :got:foo
+      0x10, 0x02, 0x00, 0x02, // add   c16, c16, :got_lo12:foo
+      0x1d, 0x02, 0x40, 0xc2, // ldr   c29, [c16]
+      0xbd, 0x13, 0xc4, 0xc2, // ldpbr c29, [c29]
+  };
+  memcpy(buf, pltData, sizeof(pltData));
+
+  uint64_t gotPltEntryAddr = sym.getGotPltVA();
+  relocateNoSym(buf + 0, R_MORELLO_DESC_ADR_PREL_PG_HI20,
+                getAArch64Page(gotPltEntryAddr) - Out::descPhdr->firstSec->addr);
+  relocateNoSym(buf + 4, R_AARCH64_ADD_ABS_LO12_NC, gotPltEntryAddr);
+}
+
 static TargetInfo *getTargetInfo() {
   if (config->morelloC64Plt) {
-    static AArch64C64 t;
-    return &t;
+    if (config->isCheriFnDesc) {
+      static AArch64C64DescABI t;
+      return &t;
+    } else {
+      static AArch64C64 t;
+      return &t;
+    }
   }
   if (config->andFeatures & (GNU_PROPERTY_AARCH64_FEATURE_1_BTI |
                              GNU_PROPERTY_AARCH64_FEATURE_1_PAC)) {

lld/ELF/Arch/Cheri.cpp
@@ -945,16 +945,42 @@ static void addCapDynamicRelocation(RelType dynType, Symbol *sym,
                                     int64_t addend) {
   bool isExecRel =
       (sym->isFunc() || sym->isGnuIFunc()) &&
-      (dynType == R_MORELLO_RELATIVE || dynType == R_MORELLO_IRELATIVE);
+      (dynType == R_MORELLO_RELATIVE || dynType == target->iRelativeRel);
+
+  RelType realDynType = dynType;
+
+  if (dynType == R_MORELLO_RELATIVE && config->isCheriFnDesc) {
+    bool isDescSym = false;
+    if (sym->getOutputSection())
+      isDescSym =
+          (sym->getOutputSection()->getPhdrFlags() & PF_W) != 0 &&
+          !sym->getOutputSection()->name.startswith(".data.rel.ro") &&
+          !sym->getOutputSection()->name.startswith(".gcc_except_table");
+    bool isDescFragment = (sec->flags & SHF_WRITE) &&
+                          !sec->name.startswith(".data.rel.ro") &&
+                          !sec->name.startswith(".gcc_except_table");
+    realDynType = R_MORELLO_RELATIVE;
+    if ((sym->isFunc() || sym->isGnuIFunc()) && addend == 0) {
+      realDynType = R_MORELLO_DESC_FUNC_RELATIVE;
+      assert(isDescFragment && "invalid function relocation");
+    } else if (isDescSym && isDescFragment) {
+      realDynType = R_MORELLO_DESC_DAT_RELATIVE;
+    } else if (isDescFragment && !isDescSym) {
+      realDynType = R_MORELLO_DESC_RELATIVE;
+    } else if (isDescSym & !isDescFragment) {
+      llvm_unreachable("invalid relocation");
+    }
+  }
+
   if (dynType == R_MORELLO_RELATIVE && !sym->includeInDynsym() &&
       config->localCapRelocsMode == CapRelocsMode::ElfReloc) {
     in.relaDyn->addReloc(
-        {dynType, sec, offset,
+        {realDynType, sec, offset,
          isExecRel ? DynamicReloc::AArch64ExecRel : DynamicReloc::AgainstSymbol,
          *sym, addend, R_ABS});
   } else {
     mainPart->relaDyn->addReloc(
-        {dynType, sec, offset,
+        {realDynType, sec, offset,
          isExecRel ? DynamicReloc::AArch64ExecRel : DynamicReloc::AgainstSymbol,
          *sym, addend, R_ABS});
   }
@@ -970,7 +996,7 @@ void addMorelloC64GotRelocation(RelType dynType, Symbol *sym,
   // If there is a Dynamic Symbol Table, there cannot be a caprelocs section.
   // R_MORELLO_IRELATIVE can be present even without a Dynamic Symbol Table
   // being present.
-  if (config->hasDynSymTab || dynType == R_MORELLO_IRELATIVE ||
+  if (config->hasDynSymTab || dynType == target->iRelativeRel ||
       config->localCapRelocsMode == CapRelocsMode::ElfReloc) {
     addCapDynamicRelocation(dynType, sym, sec, offset, addend);
   } else {

lld/ELF/Config.h
@@ -378,6 +378,9 @@ struct Configuration {
   // True if we are creating a pure-capability CheriABI output.
   bool isCheriAbi = false;
 
+  // True if we are using the function descriptor ABI.
+  bool isCheriFnDesc = false;
+
   // We need to calculate the PCC to decide how to align the OutputSections
   // on the boundary of the PCC range. Cache the calculation here so that we
   // don't need to recalculate later.

lld/ELF/Driver.cpp
@@ -2344,7 +2344,7 @@ static uint32_t getAndFeatures() {
 }
 
 template <class ELFT> static void readCheriVariants() {
-  if (!config->isCheriAbi)
+  if (!config->isCheriAbi && !config->morelloC64Plt)
     return;
 
   for (InputFile *f : objectFiles) {
@@ -2629,6 +2629,15 @@ void LinkerDriver::link(opt::InputArgList &args) {
   // contain a hint to tweak linker's and loader's behaviors.
   config->andFeatures = getAndFeatures();
 
+  if (config->morelloC64Plt) {
+    // We need to read the Cheri notes in order to properly create the target.
+    invokeELFT(readCheriVariants);
+    config->isCheriFnDesc =
+        (config->cheriVariants.lookup(NT_CHERI_GLOBALS_ABI) == CHERI_GLOBALS_ABI_FDESC);
+    if (config->isCheriFnDesc)
+      config->localCapRelocsMode = CapRelocsMode::ElfReloc;
+  }
+
   // The Target instance handles target-specific stuff, such as applying
   // relocations or writing a PLT section. It also contains target-dependent
   // values such as a default image base address.
@@ -2636,10 +2645,16 @@ void LinkerDriver::link(opt::InputArgList &args) {
 
   config->eflags = target->calcEFlags();
   config->isCheriAbi = target->calcIsCheriAbi();
-  invokeELFT(readCheriVariants);
-  if (config->emachine == EM_AARCH64)
+  if (!config->morelloC64Plt)
+    invokeELFT(readCheriVariants);
+  if (config->isCheriAbi)
+    config->isCheriFnDesc =
+        (config->cheriVariants.lookup(NT_CHERI_GLOBALS_ABI) == CHERI_GLOBALS_ABI_FDESC);
+  if (config->emachine == EM_AARCH64) {
     config->morelloPurecapBenchmarkABI = static_cast<bool>(
         config->cheriVariants.lookup(NT_CHERI_MORELLO_PURECAP_BENCHMARK_ABI));
+  }
+
   // maxPageSize (sometimes called abi page size) is the maximum page size that
   // the output can be run on. For example if the OS can use 4k or 64k page
   // sizes then maxPageSize must be 64k for the output to be useable on both.

lld/ELF/ICF.cpp
@@ -167,6 +167,7 @@ static bool isEligible(InputSection *s) {
   // Don't merge writable sections. .data.rel.ro sections are marked as writable
   // but are semantically read-only.
   if ((s->flags & SHF_WRITE) && s->name != ".data.rel.ro" &&
+      !s->name.startswith(".desc.data.rel.ro.") &&
       !s->name.startswith(".data.rel.ro."))
     return false;
 

lld/ELF/InputSection.cpp
@@ -526,6 +526,8 @@ static uint64_t getAArch64UndefinedRelativeWeakVA(uint64_t type, uint64_t p) {
   // instruction, this is 4 bytes on from P.
   case R_MORELLO_CALL26:
   case R_MORELLO_JUMP26:
+  case R_MORELLO_DESC_GLOBAL_CALL26:
+  case R_MORELLO_DESC_GLOBAL_JUMP26:
     // FIXME: return 5 rather than 4 bytes so that the relocation code
     // identifies the address as C64 and not AArch64. When relocateNoSym() can
     // examine the Symbol type we can remove this line.
@@ -712,6 +714,11 @@ uint64_t InputSectionBase::getRelocTargetVA(const InputFile *file, RelType type,
     return getAArch64Page(sym.getGotVA() + a) - getAArch64Page(p);
   case R_AARCH64_GOT_PAGE:
     return sym.getGotVA() + a - getAArch64Page(in.got->getVA());
+  case R_MORELLO_DESC_GOT_PAGE_PC:
+    // return value is Page(G(GDAT(S+A)))-Page(D)
+    // D is the address of section .descdata, where "sym" is located.
+    return getAArch64Page(sym.getGotVA() + a) -
+           getAArch64Page(Out::descPhdr->firstSec->addr);
   case R_GOT_PC:
   case R_RELAX_TLS_GD_TO_IE:
     return sym.getGotVA() + a - p;
@@ -756,6 +763,12 @@ uint64_t InputSectionBase::getRelocTargetVA(const InputFile *file, RelType type,
   case R_AARCH64_PAGE_PC: {
     uint64_t val = sym.isUndefWeak() ? p + a : sym.getVA(a);
     return getAArch64Page(val) - getAArch64Page(p);
+  }
+  case R_MORELLO_DESC_PAGE_PC: {
+    // return value is Page(S+A)-Page(D)
+    // D is the address of section where "sym" is located.
+    uint64_t val = sym.isUndefWeak() ? p + a : sym.getVA(a);
+    return getAArch64Page(val) - getAArch64Page(Out::descPhdr->firstSec->addr);
   }
   case R_RISCV_PC_INDIRECT: {
     if (const Relocation *hiRel = getRISCVPCRelHi20(&sym, a))

lld/ELF/LinkerScript.cpp
@@ -107,7 +107,7 @@ static StringRef getOutputSectionName(const InputSectionBase *s) {
   for (StringRef v :
        {".data.rel.ro", ".data", ".rodata", ".bss.rel.ro", ".bss",
         ".gcc_except_table", ".init_array", ".fini_array", ".tbss", ".tdata",
-        ".ARM.exidx", ".ARM.extab", ".ctors", ".dtors"})
+        ".ARM.exidx", ".ARM.extab", ".ctors", ".dtors", ".desc.data.rel.ro"})
     if (isSectionPrefix(v, s->name))
       return v;
 

CTSRD-CHERI/morello-llvm-project

commit 9eb1b49b01fa70356c195403b8e85d46ca12b1f3

[NFC][ELF] Remove unused and unimplemented prototype
This was never used, but presumably an earlier version of the descriptor
ABI implementation used it.

Fixes: 340b42eebe59 ("[Morello] Add lld support for the descriptor ABI.")

lld/ELF/SyntheticSections.h
@@ -1315,8 +1315,7 @@ template <typename ELFT> void writePhdrs(uint8_t *buf, Partition &part);
 
 Defined *addSyntheticLocal(StringRef name, uint8_t type, uint64_t value,
                            uint64_t size, InputSectionBase &section);
-Defined *addGlobalFunc(Symbol &sym, uint64_t value, uint64_t size,
-                       InputSectionBase &section);
+
 void addVerneed(Symbol *ss);
 
 // Linker generated per-partition sections.