rems-project/llvm-project

commit 61c84c16ae7e1468e7c12fd33098362081d68335

Add support for Morello to clang, llvm, lld, lldb, libcxx, libcxxabi and compiler-rt.
Known limitations:
  - Only the small code model is supported for the pure capability ABI
  - ELF is the only supported binary format
  - Only emulated TLS is supported in the pure capability ABI
  - No support for sanitizers
  - Dynamic linking in the pure capability ABI is supported but untested
  - IFUNC support for the pure capability ABI is untested
  - The ptrace interface support in lldb likely still needs to be modified
    to match what the kernel implements
  - lldb is missing support for decoding capabilities and will only print
    the hex value

Several people have contributed to this patch:
 Alexandros Lamprineas
 Amilendra Kodithuwakku - Morello lld support
 Arnaud de Grandmaison
 Diana Picus (diana.picus@linaro.org) - Morello lldb support
 Javed Absar
 John Brawn
 Peter Smith (peter.smith@linaro.org) - Original lld implementation
 Petr Pavlu - Generic lldb capability support, DWARF
 Silviu Baranga

Change-Id: Ie245d570b09af77b8df45928389cb54cc6ca69c9

llvm/lib/Target/AArch64/AArch64TargetObjectFile.cpp
@@ -8,12 +8,15 @@
 
 #include "AArch64TargetObjectFile.h"
 #include "AArch64TargetMachine.h"
+#include "MCTargetDesc/AArch64TargetStreamer.h"
 #include "llvm/BinaryFormat/Dwarf.h"
 #include "llvm/IR/Mangler.h"
 #include "llvm/MC/MCContext.h"
 #include "llvm/MC/MCExpr.h"
 #include "llvm/MC/MCStreamer.h"
 #include "llvm/MC/MCValue.h"
+#include "llvm/CodeGen/AsmPrinter.h"
+
 using namespace llvm;
 using namespace dwarf;
 
@@ -24,6 +27,21 @@ void AArch64_ELFTargetObjectFile::Initialize(MCContext &Ctx,
   // AARCH64 ELF ABI does not define static relocation type for TLS offset
   // within a module.  Do not generate AT_location for TLS variables.
   SupportDebugThreadLocalLocation = false;
+}
+
+TailPaddingAmount AArch64_ELFTargetObjectFile::
+getTailPaddingForPreciseBounds(uint64_t Size) const {
+  uint64_t Pad = AArch64TargetStreamer::getTargetSizeAlignReq(Size).first - Size;
+  return static_cast<TailPaddingAmount>(Pad);
+}
+
+Align AArch64_ELFTargetObjectFile::
+getAlignmentForPreciseBounds(uint64_t Size) const {
+  unsigned LogAlign =
+      AArch64TargetStreamer::getTargetSizeAlignReq(Size).second;
+  if (!LogAlign)
+    return Align::None();
+  return Align(1 << LogAlign);
 }
 
 AArch64_MachoTargetObjectFile::AArch64_MachoTargetObjectFile()

CTSRD-CHERI/morello-llvm-project

commit edc7bce5a6efbb732ac1413ba027c2e56e9eb3fc

[AArch64] Don't pad/align globals when not targeting Morello
For plain AArch64 we should not change our behaviour and instead follow
the normal upstream approach of leaving globals untouched rather than
padding and overaligning them for representable capability bounds. Aside
from being unnecessary, it breaks building upstream FreeBSD kernels due
to how the build system extracts constants to use in assembly code from
C (which has been adapted downstream in CheriBSD to not cause issues due
to capability representation, and therefore this issue does not trip up
plain AArch64 CheriBSD kernels).

Fixes: 61c84c16ae7e ("Add support for Morello to clang, llvm, lld, lldb, libcxx, libcxxabi and compiler-rt.")

llvm/lib/Target/AArch64/AArch64ISelLowering.cpp
@@ -25793,6 +25793,10 @@ static bool isLegalAtomicType(Type *Ty, const DataLayout &DL) {
 
 TailPaddingAmount AArch64TargetLowering::
 getTailPaddingForPreciseBounds(uint64_t Size) const {
+  // XXX: Other CHERI architectures only pad for purecap, not hybrid
+  if (!Subtarget->hasMorello())
+    return TailPaddingAmount::None;
+
   uint64_t Pad =
       AArch64TargetStreamer::getTargetSizeAlignReq(Size).first - Size;
   return static_cast<TailPaddingAmount>(Pad);
@@ -25800,6 +25804,10 @@ getTailPaddingForPreciseBounds(uint64_t Size) const {
 
 Align AArch64TargetLowering::
 getAlignmentForPreciseBounds(uint64_t Size) const {
+  // XXX: Other CHERI architectures only align for purecap, not hybrid
+  if (!Subtarget->hasMorello())
+    return Align();
+
   unsigned LogAlign =
     AArch64TargetStreamer::getTargetSizeAlignReq(Size).second;
   if (!LogAlign)

llvm/lib/Target/AArch64/AArch64TargetObjectFile.cpp
@@ -39,12 +39,22 @@ void AArch64_ELFTargetObjectFile::Initialize(MCContext &Ctx,
 
 TailPaddingAmount AArch64_ELFTargetObjectFile::
 getTailPaddingForPreciseBounds(uint64_t Size, const TargetMachine &TM) const {
+  // XXX: Other CHERI architectures only pad for purecap, not hybrid
+  const auto &ATM = static_cast<const AArch64TargetMachine &>(TM);
+  if (!ATM.IsMorello())
+    return TailPaddingAmount::None;
+
   uint64_t Pad = AArch64TargetStreamer::getTargetSizeAlignReq(Size).first - Size;
   return static_cast<TailPaddingAmount>(Pad);
 }
 
 Align AArch64_ELFTargetObjectFile::
 getAlignmentForPreciseBounds(uint64_t Size, const TargetMachine &TM) const {
+  // XXX: Other CHERI architectures only align for purecap, not hybrid
+  const auto &ATM = static_cast<const AArch64TargetMachine &>(TM);
+  if (!ATM.IsMorello())
+    return Align();
+
   unsigned LogAlign =
       AArch64TargetStreamer::getTargetSizeAlignReq(Size).second;
   if (!LogAlign)