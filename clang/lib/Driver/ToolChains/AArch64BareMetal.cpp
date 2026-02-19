42a46cb29dea3e33e7fc1bc4b8d7c366ba0073fe
Jessica Clarke <jrtc27@jrtc27.com>
2026-02-19 14:17:46 +0000

    [clang][Driver] Don't needlessly peek at -Wl options for --local-caprelocs
    This code is entirely unnecessary, since -Wl arguments will be added
    after this point anyway, and so will override any default values we
    provide here regardless. This code also has the weird behaviour of
    leaving the -Wl argument present whilst also updating the default, so
    clang -Wl,--local-caprelocs=foo will invoke the linker with two copies
    of --local-caprelocs=foo, one for this default, and one for the -Wl.
    Moreover, this code doesn't catch all cases of setting --local-caprelocs
    as forms like -Wl,...,--local-caprelocs=foo,... with arbitrary numbers
    of arguments either side are completely valid (though not forms where
    the flag and value are split, i.e. -Wl,...,--local-caprelocs,foo,...).

--- clang/lib/Driver/ToolChains/AArch64BareMetal.cpp
+++ clang/lib/Driver/ToolChains/AArch64BareMetal.cpp
@@ -236,15 +236,7 @@ void Linker::ConstructJob(Compilation &C, const JobAction &JA,
     CmdArgs.push_back(Args.MakeArgString(ImageBase));
 
     // Pass the linker option --local-caprelocs=elf by default.
-    std::string CapRelocsMode = "--local-caprelocs=elf";
-    for (Arg *A : Args) {
-      if (A->getOption().matches(options::OPT_Wl_COMMA) &&
-          A->getNumValues() == 1 &&
-          StringRef(A->getValue(0)).startswith("--local-caprelocs=")) {
-        CapRelocsMode = A->getValue(0);
-      }
-    }
-    CmdArgs.push_back(Args.MakeArgString(CapRelocsMode));
+    CmdArgs.push_back(Args.MakeArgString("--local-caprelocs=elf"));
   }
   // Force the search path to the targetted variant library.
   CmdArgs.push_back("-L");
