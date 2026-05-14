0d65da688252b75af6d1a7404f92a366d50c8084
John Baldwin <jhb@FreeBSD.org>
2026-05-14 16:20:18 -0400

    [ELF][c18n]: Add a flag to control implicit compartment assignment
    Implicit compartment assignment via PC-relative relocations on
    CheriABI is still enabled unconditionally since it is required to run
    at all, but this new flag controls the use of all relocations to
    implicitly assign sections to compartments.  It still defaults to
    enabled but can be disabled via --no-compartment-inference.

--- lld/ELF/Config.h
+++ lld/ELF/Config.h
@@ -485,6 +485,7 @@ struct Config {
 
   std::vector<CompartmentPolicy> compartmentPolicies;
   bool verboseCompartmentalization = false;
+  bool implicitCompartmentAssignment = true;
 };
 struct ConfigWrapper {
   Config c;
